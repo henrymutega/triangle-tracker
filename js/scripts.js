abb45133054dd8e9e4b40a6219279c01e993ce76
henrymutega <hmutega@gmail.com>
2019-06-03 06:10:36 +0300

    Add changes to scripts.js

--- js/scripts.js
+++ js/scripts.js
@@ -1,12 +1,12 @@
-var result = function(){
+  var result = function(){
   var sideA = document.getElementById('sideA').value;
   var sideB = document.getElementById('sideB').value;
   var sideC = document.getElementById('sideC').value;
 
-  var output = document.getElementById('result');
+  var output = " ";
 
    if(sideA<=0 || sideB<=0 || sideC<=0){
-    document.getElementById('result').value = output + "Invalid inputs.";
+    output = output + "Invalid inputs.";
   }else if(sideA!==sideB && sideA!==sideC && sideB!==sideC) (sideA+sideB>sideC && sideA+sideC>sideB && sideB+sideC>sideA){
     document.getElementById('result').value = output + "This is Scalene Triangle.";
   }else if(sideA===sideB && sideA===sideC && sideB===sideC){
