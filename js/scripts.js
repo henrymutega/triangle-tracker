7b437ac5c419d4c6f5e25e7348fb387baeb72c0e
henrymutega <hmutega@gmail.com>
2019-06-05 12:41:10 +0300

    Add changes to script.js

--- js/scripts.js
+++ js/scripts.js
@@ -16,15 +16,15 @@
       return;
   }
 
-   if(sides[0]<=0 || sides[1]<=0 || sides[2]<=0){
-    document.getElementById('resultOutput').innerHTML = "Invalid inputs.";
-   }else if(sides[0]+sides[1]<=sides[2] || sides[0]+sides[2]<=sides[1] || sides[1]+sides[2]<=sides[0]){
-    document.getElementById('resultOutput').innerHTML = "This is not a Triangle!";
-  }else if(sides[0]===sides[1] && sides[0]===sides[2] && sides[1]===sides[2]){
-    document.getElementById('resultOutput').innerHTML = "This is an Equilateral Triangle.";
-  }else if(sides[0]===sides[1] || sides[0]===side[2] || sides[1]===sides[2]){
-    document.getElementById('resultOutput').innerHTML = "This is an Isosceles Triangle.";
-  }else if(sides[0]!==sides[1] && sides[0]!==sides[2] && sides[1]!==sides[2]){
-    document.getElementById('resultOutput').innerHTML = "This is Scalene Triangle.";
+    if(sides[0]<=0 || sides[1]<=0 || sides[2]<=0){
+     document.getElementById('resultOutput').innerHTML = "Invalid inputs.";
+    }else if(sides[0]+sides[1]<=sides[2] || sides[0]+sides[2]<=sides[1] || sides[1]+sides[2]<=sides[0]){
+     document.getElementById('resultOutput').innerHTML = "This is not a Triangle!";
+    }else if(sides[0]!==sides[1] && sides[0]!==sides[2] && sides[1]!==sides[2]){
+      document.getElementById('resultOutput').innerHTML = "This is Scalene Triangle.";
+    }else if(sides[0]===sides[1] && sides[0]===sides[2] && sides[1]===sides[2]){
+      document.getElementById('resultOutput').innerHTML = "This is an Equilateral Triangle.";
+    }else if(sides[0]===sides[1] || sides[0]===sides[2] || sides[1]===sides[2]){
+      document.getElementById('resultOutput').innerHTML = "This is an Isosceles Triangle.";
+    }
   }
-}
